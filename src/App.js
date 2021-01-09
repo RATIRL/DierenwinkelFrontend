536a02efc2e639456ec748696e010540fae3f09f
RATIRL <sjokz@hotmail.com>
2021-01-09 14:28:14 +0100

    React-bootstrap en navigationbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductenComponent from "./ProductenComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Dierenwinkel</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <NavDropdown title="Categories" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.0">Alles</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.1">Voeding</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">Speelgoed</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">Habitat</NavDropdown.Item>
+                  </NavDropdown>
+              </Nav>
+          </Navbar.Collapse>
+      </Navbar>
   );
 }
 
---
+++ src/ProductenComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class ProductenComponent extends Component {
+
+    render(){
+        return (
+            <p> //alle producten oplijsten in deze component</p>
+
+        )};
+}
+
+export default ProductenComponent;
